8851b4ffa546e4e25fb8d703fea31846eeda7c80
libin <libin16104@autohome.com.cn>
2024-07-18 23:43:42 +0800

    添加大文件

--- src/inputs.py
+++ src/inputs.py
@@ -4,8 +4,8 @@ import tensorflow.compat.v1 as tf
 def input_fn(mode, params):
     file_pattern = params['dataset']['train_dataset'] if mode == tf.estimator.ModeKeys.TRAIN else params['dataset'][
         'test_dataset']
-    dataset = tf.data.experimental.make_csv_dataset(file_pattern=file_pattern, header=True, batch_size=16384,
-                                                    prefetch_buffer_size=16384 * 2, num_parallel_reads=32)
+    dataset = tf.data.experimental.make_csv_dataset(file_pattern=file_pattern, header=True, batch_size=8192,
+                                                    prefetch_buffer_size=1024 * 2 ** 4, num_parallel_reads=32)
     if params['train']['epoch'] > 1:
         dataset = dataset.repeat(params['train']['epoch'] - 1)
     elements = dataset.make_one_shot_iterator().get_next()
