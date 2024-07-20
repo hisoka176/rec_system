504f8225447eb3b9e0a74bcf4cf9b0a12a9796d4
libin <libin16104@autohome.com.cn>
2024-07-21 01:01:27 +0800

    添加大文件

--- src/inputs.py
+++ src/inputs.py
@@ -7,7 +7,7 @@ def input_fn(mode, params):
     dataset = tf.data.experimental.make_csv_dataset(file_pattern=file_pattern, header=True,
                                                     batch_size=params['train']['batch_size'],
                                                     prefetch_buffer_size=1024 * 2 ** 6, num_parallel_reads=32)
-    dataset = dataset.shuffle(4096).cache()
+    dataset = dataset.shuffle(1024).cache()
     if params['train']['epoch'] > 1 and mode == tf.estimator.ModeKeys.TRAIN:
         dataset = dataset.repeat(params['train']['epoch'] - 1)
     elements = dataset.make_one_shot_iterator().get_next()
